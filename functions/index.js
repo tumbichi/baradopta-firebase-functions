17ceb6024180cb6cb8e982e87220b8744fbedfe9
pity <germanviglietti@gmail.com>
2020-09-26 21:17:28 -0300

    Soporte con Node 10, y ahora la funcion tambien recibe el id del perro

--- functions/index.js
+++ functions/index.js
@@ -27,25 +27,47 @@ exports.sendNotification = functions.database.ref('/Usuarios/{uploader_id}/Notif
 		return null;
 	}
 
-	const fromUser = admin.database().ref(`Usuarios/${uploader_id}/Notifications/${solicitud_id}/from_user_id`).once('value');
+	const fromUser = admin.database().ref(`Usuarios/${uploader_id}/Notifications/${solicitud_id}`).once('value');
 
 	return fromUser.then(fromUserResult => {
-		const from_user_id = fromUserResult.val();
-		console.log('Nueva solicitud de adopcion de: ', from_user_id, 'from', fromUserResult);
+		const snapshot = fromUserResult.val();
+		const { dog_id, from_user_id } = snapshot;
+
+		console.log('dog_id', dog_id);
+		console.log('from_user', from_user_id);
+		console.log('Nueva solicitud de adopcion de: ', from_user_id, 'from', uploader_id);
 
 		const userQuery = admin.database().ref(`Usuarios/${from_user_id}/displayName`).once('value');
 		const deviceToken = admin.database().ref(`/Usuarios/${uploader_id}/device_token`).once('value');
+		const dogQuery = admin.database().ref(`Perros/${dog_id}`).once('value');
 
 
-
-		return Promise.all([userQuery, deviceToken]).then(result => {
+		return Promise.all([userQuery, deviceToken, dogQuery]).then(result => {
 			const userName = result[0].val();
 			const token_id = result[1].val();
-			console.log("Device token", token_id, " from ", userName);
+			const { nombre, etiquetas } = result[2].val();
+
+			var data;
+
+			if (etiquetas[1]){
+				data = {
+					title: `¡Nueva solicitud de adopcion!`,
+					body: `${userName} quiere adoptar a ${nombre}, ponte en contacto lo antes posible`
+				};
+			}else{
+				data = {
+					title: `¡Alguien encontro a ${nombre}!`,
+					body:`Ponte en contacto lo antes posible`
+				};
+			}
+
+			console.log('title', data['title']);
+			console.log('body', data['body']);
+
 			const payload = {
 				notification: {
-					title : 'Nueva solicitud de adopcion',
-					body : `${userName} le ha enviado una solicitud de adopcion`,
+					title : data['title'],
+					body : data['body'],
 					icon : "default",
 					click_action: "com.pity.appperros1.TARGETNOTIFICATION"
 				},
@@ -55,7 +77,7 @@ exports.sendNotification = functions.database.ref('/Usuarios/{uploader_id}/Notif
 			};
 
 			return admin.messaging().sendToDevice(token_id, payload).then(response => {
-				console.log('Funcion de notificacion');
+				console.log('¡Notificacion enviada con exito!');
 				return null;
 			});
 		});
